140fc9784b65eec4b9199d1de59662e24ae96fe8
carinasylvester <carina.sylvester@outlook.com>
2021-08-25 10:34:08 -0400

    Hello World files

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+var http = require("http")
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'})
+
+   // Send the response body as "Hello World"
+   // Modified "Hello World" to "Hello DevOps!"
+   response.end('Hello DevOps!\n')
+}).listen(3333)
+
+// Console will print the message
+console.log('Server running')
